ebab0a542b3a2298531491e6577e452bb359b00d
jagadamma <96439002+jagadamma@users.noreply.github.com>
2023-04-04 20:34:46 +0530

    Create deployment.tf

---
+++ JDApis/deployment.tf
@@ -0,0 +1,43 @@
+replicas = 2
+ selector {
+ match_labels = {
+ name = "webapp"
+ }
+ }
+ template {
+ metadata {
+ labels = {
+  name = "webapp"
+ }
+ }
+ spec {
+ container {
+ image = "jagadamma/python:latest"
+ name = "simple-webapp"
+ port {
+ container_port = 8000
+ }
+ }
+ }
+ }
+ }
+}
+
+resource "kubernetes_service" "webapp-service" {
+ metadata {
+ name = "webapp-service"
+ }
+
+ spec {
+ selector = {
+ name = "polls"
+ }
+ port {
+ port = 8000
+ target_port = 8000
+ node_port = 30080
+ }
+
+ type = "LoadBalancer"
+ }
+}
